a370f8e1e1f1ae4e082ef8ca9d8f279abf182c7e
Ivie Morrigan <iviehedera@gmail.com>
2022-04-12 10:51:24 +0100

    Update HCP Packer channels when building images

--- packer/v0.1.0/build.sh
+++ packer/v0.1.0/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
-#source ../env.sh
+set -ex
 
 packer init .
-HCP_PACKER_BUILD_FINGERPRINT=v0.1.0-$(date +%F_%H-%M-%S) packer build -force .
+export HCP_PACKER_BUILD_FINGERPRINT=v0.1.0-$(date +%F_%H-%M-%S)
+packer build -force .
+
+# Update HCP Packer Channel
+par channels set-iteration azure-webserver production --fingerprint $HCP_PACKER_BUILD_FINGERPRINT
+
+# In a real golden image pipeline, this would be set to "dev", and go through
+# a testing procedure. Once validated, it could be promoted to "production"
+#
+# For the sake of my demo however, we call v0.1.0 Production
--- packer/v0.2.0/build.sh
+++ packer/v0.2.0/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-#source ../env.sh
+set -ex
 
 packer init .
-HCP_PACKER_BUILD_FINGERPRINT=v0.2.0-$(date +%F_%H-%M-%S) packer build -force .
+export HCP_PACKER_BUILD_FINGERPRINT=v0.2.0-$(date +%F_%H-%M-%S)
+packer build -force .
+
+# Update HCP Packer Channel
+par channels set-iteration azure-webserver dev --fingerprint $HCP_PACKER_BUILD_FINGERPRINT
