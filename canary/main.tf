9b11f7c2b07237ef61c54ed8d7bf859e4b79fa28
Lucy Davinhart <lucy@lucymhdavies.com>
2023-06-12 12:18:16 +0100

    Config for Canary workspace

---
+++ canary/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  cloud {
+    organization = "fancycorp"
+
+    workspaces {
+      tags = ["webserver", "platform:azure"]
+    }
+  }
+  # Minimum provider version for OIDC auth
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.29.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.25.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+module "webserver" {
+  source = "../"
+
+  resource_group_tags = {
+    Name      = "StrawbTest"
+    Owner     = "lucy.davinhart@hashicorp.com"
+    Purpose   = "Terraform TFC Demo Org (FancyCorp)"
+    TTL       = "24h"
+    Terraform = "true"
+    Source    = "https://github.com/FancyCorp-Demo/terraform-azure-webserver/tree/main/canary"
+    Workspace = terraform.workspace
+  }
+}
